7582d46750f0723b7e838addf2eceff48e34cdc7
Michael Tichopad <michael@tichopad.dev>
2025-03-12 20:06:41 +0100

    Added TS to some packages

--- libs/utilities/src/index.ts
+++ libs/utilities/src/index.ts
@@ -4,6 +4,6 @@
  * @param {number} x - The number to double
  * @returns {number} The doubled number
  */
-export function double(x) {
+export function double(x: number): number {
   return x * 2;
 }
